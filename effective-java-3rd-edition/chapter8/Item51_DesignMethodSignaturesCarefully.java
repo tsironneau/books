f5fd139ec5c624d2cbccfec17aa16882a4615c99
tsironneau <tanguy.sironneau@gmail.com>
2020-04-15 10:06:30 +0200

    feat () : add item 51 Design method signature carefully

---
+++ effective-java-3rd-edition/chapter8/Item51_DesignMethodSignaturesCarefully.java
@@ -0,0 +1,23 @@
+package chapter8;
+
+/**
+ * <b>Item 51 : Design method signatures carefully</b>
+ * <p>Why ?</p>
+ * <ul>
+ *     <li>For usability, to make it easier to learn and remember</li>
+ *     <li>To avoid errors from client usage</li>
+ * </ul>
+ * <p>Why Not ?</p>
+ * <ul>
+ *     <li></li>
+ * </ul>
+ * <p>Things to remember</p>
+ * <ul>
+ *     <li>Choose method name carefully : must obey naming conventions, understandable and consistant, avoid long names</li>
+ *     <li>Avoid long parameter lists, four or less</li>
+ *     <li>For parameter type : use interfaces rather then classes</li>
+ *     <li>Prefer two-element enum types over boolean : easier to read and write</li>
+ * </ul>
+ */
+public class Item51_DesignMethodSignaturesCarefully {
+}
