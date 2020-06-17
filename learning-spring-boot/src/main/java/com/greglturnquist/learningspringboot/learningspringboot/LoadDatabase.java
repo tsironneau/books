c114887664e212cbfee36a828eb5317ae13a221f
tsironneau <tanguy.sironneau@gmail.com>
2020-06-17 17:18:16 +0200

    learning SB (chapter1) : add first database loader

---
+++ learning-spring-boot/src/main/java/com/greglturnquist/learningspringboot/learningspringboot/LoadDatabase.java
@@ -0,0 +1,20 @@
+package com.greglturnquist.learningspringboot.learningspringboot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+
+@Configuration
+public class LoadDatabase {
+    @Bean
+    CommandLineRunner init(final ChapterRepository chapterRepository) {
+        return args ->
+                Flux.just(
+                        new Chapter("Quick Start with Java"),
+                        new Chapter("Reactive Web with Spring Boot"),
+                        new Chapter("...and more!"))
+                    .flatMap(chapterRepository::save)
+                    .subscribe(System.out::println);
+    }
+}
