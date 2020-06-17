baeab2d2c14936d49e393b499ff70c6f12094344
tsironneau <tanguy.sironneau@gmail.com>
2020-06-17 17:08:20 +0200

    learning SB (chapter1) : add first data class and repository

---
+++ learning-spring-boot/src/main/java/com/greglturnquist/learningspringboot/learningspringboot/ChapterRepository.java
@@ -0,0 +1,6 @@
+package com.greglturnquist.learningspringboot.learningspringboot;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface ChapterRepository extends ReactiveCrudRepository<Chapter, String> {
+}
