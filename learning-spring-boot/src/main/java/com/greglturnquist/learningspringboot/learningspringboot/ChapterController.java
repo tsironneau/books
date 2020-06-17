b6431320844bc15fbf0f188b96c1290232f4013d
tsironneau <tanguy.sironneau@gmail.com>
2020-06-17 17:23:03 +0200

    learning SB (chapter1) : add a simple chapter controller

---
+++ learning-spring-boot/src/main/java/com/greglturnquist/learningspringboot/learningspringboot/ChapterController.java
@@ -0,0 +1,20 @@
+package com.greglturnquist.learningspringboot.learningspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ChapterController {
+
+    private final ChapterRepository chapterRepository;
+
+    public ChapterController(ChapterRepository chapterRepository) {
+        this.chapterRepository = chapterRepository;
+    }
+
+    @GetMapping("/chapters")
+    public Flux<Chapter> listing(){
+        return chapterRepository.findAll();
+    }
+}
